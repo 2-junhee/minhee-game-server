a36bc75838ffaa81c349d3840bcccf216f862d9c
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2024-01-09 17:02:42 +0900

    First commit

---
+++ src/main/java/site/junyo/minheegameserver/MinheeGameServerApplication.java
@@ -0,0 +1,12 @@
+package site.junyo.minheegameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinheeGameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinheeGameServerApplication.class, args);
+	}
+}
---
+++ src/test/java/site/junyo/minheegameserver/MinheeGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.junyo.minheegameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinheeGameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
