6b76506273177ff08d1d9128d619510287076280
minhee <minhee810@naver.com>
2024-02-14 14:54:54 +0900

    Feat: User entity 작성
    1. idx, uuid, id, password, nickname 필드 추가
    2. 사용자 엔티티를 데이터베이스의 'user_info' 테이블에 매핑

--- src/main/java/site/junyo/minheegame/member/domain/User.java
+++ src/main/java/site/junyo/minheegame/member/domain/User.java
@@ -1,4 +1,35 @@
 package site.junyo.minheegame.member.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "user_info")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+
+    @Column(nullable = false, length = 20, unique = true)
+    private String id;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String nickname;
+
+
 }
+
