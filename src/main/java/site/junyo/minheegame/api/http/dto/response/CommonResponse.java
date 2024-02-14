8478f826ba41a3fb38799832b708c6d7054f96b3
minhee <minhee810@naver.com>
2024-02-15 01:04:19 +0900

    Feat: CommonResponse 작성
    1. code, msg 필드 선언

--- src/main/java/site/junyo/minheegame/api/http/dto/response/CommonResponse.java
+++ src/main/java/site/junyo/minheegame/api/http/dto/response/CommonResponse.java
@@ -1,5 +1,11 @@
 package site.junyo.minheegame.api.http.dto.response;
 
-public record CommonResponse (int code, String msg){
+public class CommonResponse{
+    private final int code;
+    private final String msg;
 
+    public CommonResponse(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
 }
