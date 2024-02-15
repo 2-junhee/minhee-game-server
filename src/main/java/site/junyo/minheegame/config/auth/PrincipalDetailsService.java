0a6ef4b3f687372cf053b2513ccd96fa23b78852
minhee <minhee810@naver.com>
2024-02-15 13:30:11 +0900

    Feat: PrincipalDetails, PrincipalDetailsService 작성
    1. 로그인 로직을 위한 UserDetail 설정

---
+++ src/main/java/site/junyo/minheegame/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,30 @@
+package site.junyo.minheegame.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import site.junyo.minheegame.member.domain.User;
+import site.junyo.minheegame.member.repository.UserRepository;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        } else {
+            return new PrincipalDetails(user);
+        }
+    }
+}
