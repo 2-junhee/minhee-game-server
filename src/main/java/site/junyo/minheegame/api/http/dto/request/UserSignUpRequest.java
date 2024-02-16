cffd11df895b5593b2b1f031ddc011b36d2b27f3
minhee <minhee810@naver.com>
2024-02-16 22:52:50 +0900

    Feat: UserSignUpRequest
    1. 메시지 필드명 통일

--- src/main/java/site/junyo/minheegame/api/http/dto/request/UserSignUpRequest.java
+++ src/main/java/site/junyo/minheegame/api/http/dto/request/UserSignUpRequest.java
@@ -7,15 +7,15 @@ import lombok.Data;
 @Data
 public class UserSignUpRequest {
 
-    @NotBlank(message = "ID 입력은 필수입니다.")
+    @NotBlank(message = "아이디 입력은 필수입니다.")
     @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$", message = "유효하지 않은 아이디입니다.")
     private String id;
 
-    @NotBlank(message = "Password 입력은 필수입니다.")
+    @NotBlank(message = "비밀번호 입력은 필수입니다.")
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=!]).{8,20}$", message = "유효하지 않은 비밀번호입니다.")
     private String password;
 
-    @NotBlank(message = "Nickname 입력은 필수입니다.")
+    @NotBlank(message = "닉네임 입력은 필수입니다.")
     @Pattern(regexp = "^[a-zA-Z가-힣0-9]{2,20}$", message = "유효하지 않은 닉네임입니다.")
     private String nickname;
 
