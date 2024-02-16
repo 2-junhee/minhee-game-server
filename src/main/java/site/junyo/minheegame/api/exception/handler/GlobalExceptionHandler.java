6e09a36955131baed88c92fac58eb787ad91f94b
minhee <minhee810@naver.com>
2024-02-16 23:20:55 +0900

    Feat: GlobalException 처리 수정
    1. 에러 필드마다 각각 에러코드 다르게 응답하도록 수정
    2. ErrorResponse 추가 : 에러 발생 시 String 타입의 메시지 만으로 설명이 부족할 경우가 있어 errors 필드를 추가한 에러 응답 객체 추가

--- src/main/java/site/junyo/minheegame/api/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/site/junyo/minheegame/api/exception/handler/GlobalExceptionHandler.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import site.junyo.minheegame.api.exception.DuplicateUserIdException;
 import site.junyo.minheegame.api.http.dto.response.CommonResponse;
 
+import site.junyo.minheegame.api.http.dto.response.ErrorResponse;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -21,15 +24,38 @@ public class GlobalExceptionHandler {
     public ResponseEntity<CommonResponse> handlerMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
 
         List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
-        List<String> errors = fieldErrors.stream().map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage()).toList();
 
-        return new ResponseEntity<>(new CommonResponse(-28,"여러 항목 유효성 검사 실패 "), HttpStatus.BAD_REQUEST);
+        if (fieldErrors.size() == 1) {
+            FieldError fieldError = fieldErrors.get(0);
+            String fieldName = fieldError.getField();
+            int errorCode = getErrorCodeForField(fieldName);
+            String errorMessage = fieldError.getDefaultMessage();
+
+            return new ResponseEntity<>(new CommonResponse(errorCode, errorMessage), HttpStatus.BAD_REQUEST);
+        }
+
+        List<String> errors = fieldErrors.stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(new ErrorResponse(-28, "다중 유효성 검사 실패", errors), HttpStatus.BAD_REQUEST);
+
     }
 
+    private int getErrorCodeForField(String fieldName) {
+
+        return switch (fieldName) {
+            case "id" -> -22;
+            case "password" -> -23;
+            case "nickname" -> -24;
+            default -> -28;
+        };
+    }
 
     @ExceptionHandler(DuplicateUserIdException.class)
     public ResponseEntity<CommonResponse> handlerDuplicateUserIdException(DuplicateUserIdException ex) {
         return new ResponseEntity<>(new CommonResponse(ex.getCode(), ex.getMessage()), HttpStatus.CONFLICT);
     }
+
 
 }
