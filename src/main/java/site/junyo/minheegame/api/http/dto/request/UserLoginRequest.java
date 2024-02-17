f3bd065e5e67e255af08c4375a87de8ec927c6e4
junheiLee <jun2ee22@naver.com>
2024-02-17 11:16:23 +0900

    Feat: 로그인은 패턴에 대한 유효성 검사를 하지 않아 해당 어노테이션 삭제

--- src/main/java/site/junyo/minheegame/api/http/dto/request/UserLoginRequest.java
+++ src/main/java/site/junyo/minheegame/api/http/dto/request/UserLoginRequest.java
@@ -1,7 +1,6 @@
 package site.junyo.minheegame.api.http.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,11 +10,9 @@ import lombok.ToString;
 public class UserLoginRequest {
 
     @NotBlank(message = "ID 입력은 필수입니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$", message = "유효하지 않은 아이디입니다.")
     private String id;
 
     @NotBlank(message = "Password 입력은 필수입니다.")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=!]).{8,20}$", message = "유효하지 않은 비밀번호입니다.")
     private String password;
 
 }
