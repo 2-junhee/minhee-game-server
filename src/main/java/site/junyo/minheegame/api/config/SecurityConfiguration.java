638c55917844986661e92ad24dc92e20f6507698
junheiLee <jun2ee22@naver.com>
2024-02-21 05:48:19 +0900

    Feat: security 기본 설정

--- src/main/java/site/junyo/minheegame/api/config/SecurityConfiguration.java
+++ src/main/java/site/junyo/minheegame/api/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,12 +29,19 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http
-                .authorizeHttpRequests(authorizeHttpRequests -> {
-            authorizeHttpRequests
-                    .requestMatchers("/", "/api/v1/**").permitAll()
-                    .anyRequest().authenticated();
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
 
-        }).csrf(AbstractHttpConfigurer::disable).build();
+                .authorizeHttpRequests(authorizeHttpRequests ->
+                    authorizeHttpRequests
+                            .requestMatchers("/", "/api/v1/**").permitAll()
+                            .anyRequest().authenticated())
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .build();
     }
 
     @Bean
